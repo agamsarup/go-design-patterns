fb25484d52c6be5aaf3ad3a308758259238bc1ea
agamsarup <agamsarup@gmail.com>
2023-03-04 23:37:17 +0530

    behavioral - chain of responsibility

---
+++ behavioural/chain_of_responsibility.go
@@ -0,0 +1,91 @@
+// source - https://refactoring.guru/design-patterns/chain-of-responsibility
+// Chain of Responsibility is a behavioral design pattern that lets you pass requests along a chain of handlers.
+// Upon receiving a request, each handler decides either to process the request or to pass it to the next handler in the chain.
+package behavioural
+
+import "fmt"
+
+type department interface {
+	execute(*patient)
+	setNext(department)
+}
+
+type reception struct {
+	next department
+}
+
+func (r *reception) execute(p *patient) {
+	if p.registrationDone {
+		fmt.Println("Patient registration already done")
+		r.next.execute(p)
+		return
+	}
+	fmt.Println("Reception registering patient")
+	p.registrationDone = true
+	r.next.execute(p)
+}
+
+func (r *reception) setNext(next department) {
+	r.next = next
+}
+
+type doctor struct {
+	next department
+}
+
+func (d *doctor) execute(p *patient) {
+	if p.doctorCheckUpDone {
+		fmt.Println("Doctor checkup already done")
+		d.next.execute(p)
+		return
+	}
+	fmt.Println("Doctor checking patient")
+	p.doctorCheckUpDone = true
+	d.next.execute(p)
+}
+
+func (d *doctor) setNext(next department) {
+	d.next = next
+}
+
+type medical struct {
+	next department
+}
+
+func (m *medical) execute(p *patient) {
+	if p.medicineDone {
+		fmt.Println("Medicine already given to patient")
+		m.next.execute(p)
+		return
+	}
+	fmt.Println("Medical giving medicine to patient")
+	p.medicineDone = true
+	m.next.execute(p)
+}
+
+func (m *medical) setNext(next department) {
+	m.next = next
+}
+
+type cashier struct {
+	next department
+}
+
+func (c *cashier) execute(p *patient) {
+	if p.paymentDone {
+		fmt.Println("Payment Done")
+	}
+	fmt.Println("Cashier getting money from patient")
+}
+
+func (c *cashier) setNext(next department) {
+	c.next = next
+}
+
+type patient struct {
+	name              string
+	registrationDone  bool
+	doctorCheckUpDone bool
+	medicineDone      bool
+	paymentDone       bool
+}
