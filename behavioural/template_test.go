885990455cc31157f2e3b76c99e96f7535c8482e
agamsarup <agamsarup@gmail.com>
2023-03-04 15:55:48 +0530

    behavioral design pattern - template method

---
+++ behavioural/template_test.go
@@ -0,0 +1,22 @@
+package behavioural
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestTemplateMethod(t *testing.T) {
+	smsOTP := &Sms{}
+	o := Otp{
+		iOtp: smsOTP,
+	}
+	o.genAndSendOTP(4)
+
+	fmt.Println("")
+	emailOTP := &Email{}
+	o = Otp{
+		iOtp: emailOTP,
+	}
+	o.genAndSendOTP(4)
+
+}
