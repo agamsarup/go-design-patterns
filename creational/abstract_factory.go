5a3cdea7cde84e5c0921111457e0b9c4ed246f0a
agamsarup <agamsarup@gmail.com>
2023-03-04 22:09:41 +0530

    creational - abstract factory

---
+++ creational/abstract_factory.go
@@ -0,0 +1,138 @@
+// https://refactoring.guru/design-patterns/abstract-factory/go/example
+// Abstract Factory is a creational design pattern, which solves the problem of
+// creating entire product families without specifying their concrete classes.
+// It lets you create a family of related objects. It is an abstraction over the factory pattern.
+package creational
+
+import "fmt"
+
+type ISportsFactory interface {
+	makeShoe() IShoe
+	makeShirt() IShirt
+}
+
+func GetSportsFactory(brand string) (ISportsFactory, error) {
+	if brand == "adidas" {
+		return &Adidas{}, nil
+	}
+
+	if brand == "nike" {
+		return &Nike{}, nil
+	}
+
+	return nil, fmt.Errorf("wrong brand type passed")
+}
+
+type Adidas struct {
+}
+
+func (a *Adidas) makeShoe() IShoe {
+	return &AdidasShoe{
+		Shoe: Shoe{
+			logo: "adidas",
+			size: 14,
+		},
+	}
+}
+
+func (a *Adidas) makeShirt() IShirt {
+	return &AdidasShirt{
+		Shirt: Shirt{
+			logo: "adidas",
+			size: 14,
+		},
+	}
+}
+
+type Nike struct {
+}
+
+func (n *Nike) makeShoe() IShoe {
+	return &NikeShoe{
+		Shoe: Shoe{
+			logo: "nike",
+			size: 14,
+		},
+	}
+}
+
+func (n *Nike) makeShirt() IShirt {
+	return &NikeShirt{
+		Shirt: Shirt{
+			logo: "nike",
+			size: 14,
+		},
+	}
+}
+
+type IShoe interface {
+	setLogo(logo string)
+	setSize(size int)
+	getLogo() string
+	getSize() int
+}
+
+type Shoe struct {
+	logo string
+	size int
+}
+
+func (s *Shoe) setLogo(logo string) {
+	s.logo = logo
+}
+
+func (s *Shoe) getLogo() string {
+	return s.logo
+}
+
+func (s *Shoe) setSize(size int) {
+	s.size = size
+}
+
+func (s *Shoe) getSize() int {
+	return s.size
+}
+
+type AdidasShoe struct {
+	Shoe
+}
+
+type NikeShoe struct {
+	Shoe
+}
+
+type IShirt interface {
+	setLogo(logo string)
+	setSize(size int)
+	getLogo() string
+	getSize() int
+}
+
+type Shirt struct {
+	logo string
+	size int
+}
+
+func (s *Shirt) setLogo(logo string) {
+	s.logo = logo
+}
+
+func (s *Shirt) getLogo() string {
+	return s.logo
+}
+
+func (s *Shirt) setSize(size int) {
+	s.size = size
+}
+
+func (s *Shirt) getSize() int {
+	return s.size
+}
+
+type AdidasShirt struct {
+	Shirt
+}
+
+type NikeShirt struct {
+	Shirt
+}
